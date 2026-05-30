f3b43b67e96299da6cddb1ec1e0943334727aa56
Stameska123 <postkirgizia@gmail.com>
2026-05-30 03:22:49 +0300

    3

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.happybirthday.ui.theme.HappyBirthdayTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,13 +35,17 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GreetingText(message: String, modifier: Modifier = Modifier){
-    Text(text = message)
+    Text(
+        text = message,
+        fontSize = 100.sp,
+        lineHeight = 116.sp
+        )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun BirthdayCardPrewview() {
     HappyBirthdayTheme {
-
+        GreetingText(message = "Happy Birthday Sam!")
     }
 }
